03d8c9b090fa7d28b70f523daa497cacc1a36793
Pariharx7 <parihar.peace@gmail.com>
2024-11-14 20:05:55 +0530

    Added proxyserver.c file

--- proxyserver.c
+++ proxyserver.c
@@ -31,7 +31,7 @@ cache_element* find(char* url);
 int add_cache_element(char* data, int size, char* = url);
 void remove_cache_element();
 
-int port = 8080;
+int port_number = 8080;
 int proxy_socketId;
 pthread_t tid[MAX_CLIENTS];
 sem_t semaphore;
@@ -41,7 +41,31 @@ cache_element* head;
 int cache_size;
 
 int main(int argc, char* argv[]){
-    
+    int client_socketId, client_len;
+    struct sockaddr_in server_addr, client_addr;
+    sem_init(&semaphore,0, MAX_CLIENTS);
+    pthread_mutex_init(&lock, NULL);
+    if(argv == 2){
+        port_number = atoi(argv[1]);
+    }else{
+        printf("Too few arguments\n");
+        exit(1);
+    }
+
+    printf("Starting Proxy Server at port: %d\n", port_number);
+    proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
+    if(proxy_socketId < 0){
+        perror("Failed to create a socket\n");
+        exit(1);
+    }
+    int reuse = 1;
+    if(setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR,(const char*)&reuse, sizeof(reuse))< 0){
+        perror("SetSockOpt failed\n");
+    }
+    bzero((char*)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);
+
 }
 
  
