4356edcb297edc7ac058f993dd77ee99ee162471
Pariharx7 <parihar.peace@gmail.com>
2024-11-14 20:22:33 +0530

    Added proxyserver.c file

--- proxyserver.c
+++ proxyserver.c
@@ -65,7 +65,44 @@ int main(int argc, char* argv[]){
     bzero((char*)&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port_number);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(proxy_socketId,(struct sockaddr*)&server_addr, sizeof(server_addr) < 0)){
+        perror("Port is not available\n");
+        exit(1);
+    }
+    printf("Binding on port %d\n", port_number);
+    int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+    if(listen_status < 0){
+        perror("Error in listening\n");
+        exit(1);
+    }
 
+    int i = 0;
+    int Connected_socketId[MAX_CLIENTS];
+    while(1){
+        bzero((char *)&client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+        client_socketId = accept(proxy_socketId, (struct socketaddr *)&client_addr, (socklen_t*)&client_len);
+
+        if(client_socketId<0){
+            printf("Not able to connect\n");
+            exit(1);
+        }
+        else{
+            Connected_socketId[i] = client_socketId;
+        }
+
+        sockaddr_in * client_pt = (struct sockaddr_in *)*client_addr;
+        struct in_addr ip_addr = client_pt -> sin_addr;
+        char str[INET_ADDRSTRLEN];
+        inet_ntop[AF_INET, &ip_addr, str, INET_ADDRSTRLEN];
+        printf("Client is connected with port number %d and ip address is %s\n",ntohs(client_addr.sin_port),str);
+
+        pthread_create(&tid[i], NULL, thread_fn, (void *)&Connected_socketId[i]);
+        i++;
+    }
+    close(proxy_socketId);
+    return 0;
 }
 
  
